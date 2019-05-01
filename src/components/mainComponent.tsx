c2041d916f878a69e84201c2a1556a60d240884c
Airscript <f.sardonej@gmail.com>
2019-05-01 20:17:10 +0200

    repo: genesis commit

---
+++ src/components/mainComponent.tsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>Everything is working properly.</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Main;
