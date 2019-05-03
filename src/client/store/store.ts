175cf403be0e72804eb4135d31db4faec0a29bf0
Francesco Sardone <francesco@airscript.it>
2019-05-03 22:06:31 +0200

    feat(client): add store.ts

---
+++ src/client/store/store.ts
@@ -0,0 +1,29 @@
+/**
+ * Importing the modules needed for the store.
+ */
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+/**
+ * Combining all the reducers together.
+ */
+const rootReducer = combineReducers({
+
+});
+
+/**
+ * Exporting AppState: it contains the typeof rootReducer that 
+ * will be used into mapStateToProps.
+ */
+export type AppState = ReturnType<typeof rootReducer>;
+
+/**
+ * Configuring the Redux Store.
+ */
+export const configureStore = () => {
+    const store = createStore(
+        rootReducer,
+        applyMiddleware(thunk, logger)
+    );
+};
