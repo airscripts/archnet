52b4c094b68278e22aec19b18237eb3dcf32bc91
Francesco Sardone <francesco@airscript.it>
2019-06-15 01:20:03 +0200

    feat(server): add 404 and 405 pug error routes

--- src/server/routes/rootRoute.ts
+++ src/server/routes/rootRoute.ts
@@ -19,10 +19,22 @@ const rootRouter = express.Router();
  * Creating the endpoint.
  */
 rootRouter.route('/')
+/**
+ * Checking if there is a request on other not concerned HTTP methods.
+ */
+.all((req: any, res: any, next: any) => {
+  if(req.method !== "GET" && req.method !== "POST" && req.method !== "PUT" && req.method !== "DELETE") {
+    res.status(405);
+    res.render("405", {error: res.statusCode, method: req.method});
+  }
+
+  next();
+})
+
 .get(async (req: any, res: any, next: any) => {
   if(req.header("Content-Type") != "application/json") {
     res.status(405);
-    res.render("405", {error: res.statusCode});
+    res.render("405", {error: res.statusCode, method: req.method});
   }
 
   else {
@@ -35,17 +47,17 @@ rootRouter.route('/')
 
 .post((req: any, res: any, next: any) => {
   res.status(405);
-  res.render("405", {error: res.statusCode});
+  res.render("405", {error: res.statusCode, method: req.method});
 })
 
 .put((req: any, res: any, next: any) => {
   res.status(405);
-  res.render("405", {error: res.statusCode});
+  res.render("405", {error: res.statusCode, method: req.method});
 })
 
 .delete((req: any, res: any, next: any) => {
   res.status(405);
-  res.render("405", {error: res.statusCode});
+  res.render("405", {error: res.statusCode, method: req.method});
 });
 
 module.exports = rootRouter;
