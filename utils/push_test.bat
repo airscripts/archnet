6a90803cf22c67e36dd9e257a2d4c504cef624dd
Francesco Sardone <francesco@airscript.it>
2019-07-01 10:18:06 +0200

    feat: add push utilities

---
+++ utils/push_test.bat
@@ -0,0 +1,31 @@
+@echo off
+:: This batch file is used to commit and push to development branch.
+
+:: Setting the commit message.
+set commitMessage=%1
+
+echo Hello, %USERNAME%!
+echo.
+
+echo -------------------------------------------------------
+echo.
+
+:: Compiling server.ts
+echo Pushing to development branch...
+cd C:\Users\%USERNAME%\dev\github\archnet
+
+echo.
+dir
+echo.
+
+call git add .
+call git status
+call git commit -m %commitMessage%
+call git push -u origin test
+echo Git push completed to development branch!
+
+echo.
+echo -------------------------------------------------------
+echo.
+
+pause
