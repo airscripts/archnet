d7d290066374f6e7840743d777eb6ad8dc9d7179
Airscript <francesco@airscript.it>
2023-01-20 00:14:01 +0100

    refactor: huge clean up inside repl.ts

--- repl.ts
+++ repl.ts
@@ -1,44 +1,38 @@
 import repl from "repl";
 import chalk from "chalk";
+
 import { getIp } from "./modules/ip";
 
-console.log(chalk.blue(
-  "\nWelcome to Archnet!"
-  + "\nI am Archie, and I'm here to help you!"
-  + "\nType .help to check what I can do for you. :)\n"
-));
+console.log(chalk.blue(`
+Welcome to Archnet!
+Type .help to check what I can do for you.
+`));
 
 const replServer = repl.start({
-  prompt: "$ Archnet: ",
+  prompt: "archnet > ",
   input: process.stdin,
   output: process.stdout,
 });
 
 replServer.on('exit', () => {
-  console.log(chalk.blue("Come back whenever you want to! :)"));
+  console.log(chalk.blue("Come back whenever you want to!"));
   process.exit();
 });
 
 replServer.defineCommand('cls', {
   help: "Using .cls will clear the console.",
+
   action() {
     console.clear();
-
-    console.log(chalk.blue(
-      "\nWelcome to Archnet!"
-      + "\nI am Archie, and I'm here to help you!"
-      + "\nType .help to check what can I do for you. :)\n"
-    ));
-
     this.displayPrompt();
   }
 });
 
 replServer.defineCommand('ip', {
-  help: "It gives you back your current IP Address.",
+  help: "Check your current IP address.",
+
   async action() {
     await getIp();
-    console.log("");
     this.displayPrompt();
   }
 });
