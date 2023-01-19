94d0d6779edcfaa17e4a515f09d5a6bf554b1534
Airscript <francesco@airscript.it>
2023-01-19 23:49:59 +0100

    refactor: clean up server.ts

--- server.ts
+++ server.ts
@@ -1,69 +1,19 @@
-/*
- * Defining the modules required for the server.
- */
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const pug = require("pug");
+import cors from "cors";
+import express from "express";
+import ipRouter from "./api/ip";
+import { Request, Response,NextFunction } from "express";
 
-/**
- * Importing routers.
- */
-const rootRouter = require("./src/server/routes/rootRoute");
-const ipRouter = require("./src/server/api/ipApi");
-
-/*
- * Initializing the Express App.
- */
 const app = express();
-
-/**
- * Initializing pug.
- */
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "./src/server/views"));
-
-/*
- * Defining useful constants.
- */
 const port = process.env.PORT || 8080;
 
-/**
- * Setting the core endpoints.
- */
-app.use("/root", rootRouter);
-
-
-/**
- * Setting the api endpoints.
- */
 app.use("/api/ip", ipRouter);
-
-/**
- * Setting up the use of CORS Policy.
- */
 app.use(cors());
 app.options('*', cors());
 
-/*
- * Defining static files directory root.
- */
-app.use(express.static(path.join(__dirname, "build")));
-
-/*
- * Redirecting all the routes that are not defined.
- */
-app.use(function(err: any, req: any, res: any, next: any) {
-  return res.status(500).render("500", { error: res.statusCode });
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  return res.status(500);
 });
 
-app.use(function(req: any, res: any, next: any) {
-  return res.status(404).render("404", { error: res.statusCode });
-});
-
-/*
- * Setting the Express App to listen state.
- */
 app.listen(port, () => {
-  console.log(`Archie is listening on ${port}.`);
+  console.log(`Archnet is listening on ${port}.`);
 });
