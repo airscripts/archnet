053bc9e1f18da45bbbb8c67b9ab1bc5d988e97ea
Airscript <francesco@airscript.it>
2023-01-19 22:59:36 +0100

    refactor: remove client from project

--- src/client/scripts/sleep.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The sleep function takes a number as a paramter and indicates the 
- * seconds that the runtime as to wait till continue.
- */
-export default function sleep(ms: number) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
-}
